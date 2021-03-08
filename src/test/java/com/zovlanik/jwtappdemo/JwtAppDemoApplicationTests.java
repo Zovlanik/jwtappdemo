7dd4067fa224e3ecd6b53a6802bcc2762cf2567a
Zovlanik <Dkflbvbh162>
2021-03-08 14:51:39 +0300

    Неудачные попытки запуститься

---
+++ src/test/java/com/zovlanik/jwtappdemo/JwtAppDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zovlanik.jwtappdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtAppDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
